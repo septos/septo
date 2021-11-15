d83fed295cbb578f31840628bac6422fb5934469
laptop_septo <mistersepto@gmail.com>
2021-11-15 16:54:51 +0700

    membaca json page

--- index.js
+++ index.js
@@ -14,4 +14,17 @@ const page = (
         </ol>
     </div>
 )
-ReactDOM.render(page, document.getElementById('root'))
+const page = (
+    <div>
+        <h1>My awesome website in React</h1>
+        <h3>Reasons I love React</h3>
+        <ol>
+            <li>It's composable</li>
+            <li>It's declarative</li>
+            <li>It's a hireable skill</li>
+            <li>It's actively maintained by skilled people</li>
+        </ol>
+    </div>
+)
+
+document.getElementById("root").append(JSON.stringify(page))
