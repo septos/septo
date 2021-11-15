04d3f5ab69a25b7909e0bb73fe845f720f83ab01
laptop_septo <mistersepto@gmail.com>
2021-11-15 15:27:15 +0700

    memasukan barisan div kedalam variabel di js

--- index.js
+++ index.js
@@ -3,4 +3,18 @@
 // h1.className = "header"
 // console.log(h1)
 
-ReactDOM.render(<h1>This is JSX</h1>, document.getElementById("root"))
+// ReactDOM.render(<h1 className="header">This is JSX</h1>, document.getElementById("root"))
+
+
+
+const page = (
+	<div>
+		<h1 className="header">This is JSX</h1>
+		<p>This is a paragraph</p>
+		</div>
+) // memasukan ke dalam variable
+
+ReactDOM.render(
+	page,
+	document.getElementById("root")
+)
