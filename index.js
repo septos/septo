73f4b56c4d7662fb0386ecaa5bc6801c6a5c856f
laptop_septo <mistersepto@gmail.com>
2021-11-15 15:40:03 +0700

    latihan chalange navbar di masukan ke dalam variabel di render

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
-// const h1 = document.createElement("h1")
-// h1.textContent = "Hello world"
-// h1.className = "header"
-// console.log(h1)
+// const page = (
+//     <div>
+//         <h1 className="header">This is JSX</h1>
+//         <p>This is a paragraph</p>
+//     </div>
+// )
 
-// ReactDOM.render(<h1 className="header">This is JSX</h1>, document.getElementById("root"))
+/* 
+Challenge: 
+
+Create a navbar in JSX:
+    - Use the semantic `nav` element as the parent wrapper
+    - Have an h1 element with the brand name of your "website"
+    - Insert an unordered list for the other nav elements
+        - Inside the `ul`, have three `li`s for "Pricing",
+        "About", and "Contact"
+    - Don't worry about styling yet - it'll just be plain-looking HTML for now
+*/
 
 
-
-const page = (
-	<div>
-		<h1 className="header">This is JSX</h1>
-		<p>This is a paragraph</p>
-		</div>
-) // memasukan ke dalam variable
-
-ReactDOM.render(
-	page,
-	document.getElementById("root")
+const navbar = (
+    <nav>
+        <h1 align="center">Selamat datang di website Pasopati</h1>
+        <ul>
+            <li>Home</li>
+            <li>Profile</li>
+            <li>About</li>
+            <li>Contact us</li>
+        </ul>
+    </nav>
 )
+
+ReactDOM.render(navbar, document.getElementById('root'))
